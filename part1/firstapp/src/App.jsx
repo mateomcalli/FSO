1e7bea3efa85d6b937e805bf925d7dc529f0e237
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-04-29 12:34:24 -0400

    Exercise 1.1

--- part1/firstapp/src/App.jsx
+++ part1/firstapp/src/App.jsx
@@ -1,3 +1,25 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.name}, {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Total exercises {props.total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +31,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content name={part1} exercises={exercises1}/>
+      <Content name={part2} exercises={exercises2}/>
+      <Content name={part3} exercises={exercises3}/>
+      <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
