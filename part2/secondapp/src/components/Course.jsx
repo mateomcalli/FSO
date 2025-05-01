a8d563573cad842d12cc509c4f655e74977d9287
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-05-01 16:01:30 -0400

    Exercise 2.2

--- part2/secondapp/src/components/Course.jsx
+++ part2/secondapp/src/components/Course.jsx
@@ -1,8 +1,11 @@
 const Course = (props) => {
+  let sum = 0
+  props.course.parts.forEach(part => {sum += part.exercises})
   return (
     <div>
       <h1>{props.course.name}</h1>
       {props.course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      <p><strong>total of {sum} exercises</strong></p>
     </div>
   )
 }
