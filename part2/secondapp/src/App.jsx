06974c8defc706032fcd9e19526fd80991f3e0db
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-05-02 11:35:23 -0400

    Exercises 2.6-7

--- part2/secondapp/src/App.jsx
+++ part2/secondapp/src/App.jsx
@@ -1,14 +1,42 @@
-import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [newName, setNewName] = useState('')
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook.`)
+    } else {
+      console.log(newName);
+      setPersons(persons.concat({
+        name: newName
+      }))
+    }
+    setNewName('')
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick = {addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <div key={person.name}>{person.name}</div>
+      )}
     </div>
   )
 }
