5f0e40390612162fa925e516c5a2d624998d9f48
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-05-09 22:56:14 -0400

    Exercise 3.12

---
+++ part3/phonebookback/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 2) {
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://mateomcallisterschool:${password}@fso.whqcewz.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=FSO`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+if (process.argv.length === 5) {
+  person.save().then(result => {
+    console.log(`Person ${name} saved, with number ${number}`);
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 3) {
+  Person.find({}).then(persons => {
+    console.log(
+      `phonebook:\n
+      ${persons
+      .map(person => `${person.name} ${person.number}`)
+      .join('\n')
+    }`)
+    mongoose.connection.close()
+  })
+}
